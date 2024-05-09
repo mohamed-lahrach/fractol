1c4d6455757f6f90f4e2f2ff65432b064bde5a93
Mohamed Lahrach <mlahrach@c3r4p3.1337.ma>
2024-05-09 18:09:42 +0100

    correct minilbx

---
+++ fractol.h
@@ -0,0 +1,88 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdio.h> 
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include "minilibx-linux/mlx.h"
+
+
+#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value_1> <value_2>\"\n"
+
+#define WIDTH	800
+#define	HEIGHT	800
+
+
+#define BLACK       0x000000  
+#define WHITE       0xFFFFFF  
+#define RED         0xFF0000 
+#define GREEN       0x00FF00  
+#define BLUE        0x0000FF 
+
+#define MAGENTA_BURST   0xFF00FF  
+#define LIME_SHOCK      0xCCFF00  
+#define NEON_ORANGE     0xFF6600 
+#define PSYCHEDELIC_PURPLE 0x660066 
+#define AQUA_DREAM      0x33CCCC
+#define HOT_PINK        0xFF66B2 
+#define ELECTRIC_BLUE   0x0066FF 
+#define LAVA_RED        0xFF3300 
+
+
+
+typedef struct	s_complex
+{
+	double	x;
+	double	y;
+}				t_complex;
+
+
+typedef struct	s_img
+{
+	void	*img_ptr; 
+	char	*pixels_ptr; 
+	int		bpp;
+	int		endian;
+	int		line_len;
+}				t_img;
+
+
+typedef struct	s_fractal
+{
+	char	*name;
+	void	*mlx_connection; 
+	void	*mlx_window;
+	t_img	img;
+
+	double	escape_value;
+	int		iterations_defintion;
+	double	shift_x;
+	double	shift_y;
+	double	zoom;
+	double	julia_x;
+	double	julia_y;
+}				t_fractal;
+
+
+int		ft_strncmp(char *s1, char *s2, int n);
+void    putstr_fd(char *s, int fd);
+double  atodbl(char *s);
+
+void    fractal_init(t_fractal *fractal);
+
+void    fractal_render(t_fractal *fractal);
+
+double		map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
+t_complex   sum_complex(t_complex z1, t_complex z2);
+t_complex   square_complex(t_complex z);
+
+int			key_handler(int keysym, t_fractal *fractal);
+
+int    	close_handler(t_fractal *fractal);
+int		mouse_handler(int button, int x, int y, t_fractal *fractal);
+int		julia_track(int x, int y, t_fractal *fractal);
+
+#endif
---
+++ main.c
@@ -0,0 +1,27 @@
+#include "fractol.h"
+#include "minilibx-linux/mlx.h"
+
+int	main(int ac, char **av)
+{
+	t_fractal	fractal;
+
+
+	if (2 == ac && !ft_strncmp(av[1], "mandelbrot", 10)
+		|| 4 == ac && !ft_strncmp(av[1], "julia", 5))
+	{
+		fractal.name = av[1];
+		if (!ft_strncmp(fractal.name, "julia", 5))
+		{
+			fractal.julia_x = atodbl(av[2]);
+			fractal.julia_y = atodbl(av[3]);
+		}
+		fractal_init(&fractal);
+		fractal_render(&fractal);
+		mlx_loop(fractal.mlx_connection);
+	}
+	else
+	{
+		putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+}
